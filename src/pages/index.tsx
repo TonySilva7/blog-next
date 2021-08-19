6a501ac5247cd900af0b14096d04cd35e48a63b4
TonySilva7 <tony.deve7oper@gmail.com>
2021-08-19 15:31:05 -0300

    feat: implements static generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
-export default function Home() {
+import { GetStaticProps } from 'next';
+import { PostData } from '../domain/posts/post';
+
+// Faz a busca na api
+const getPosts = async (): Promise<PostData[]> => {
+  const posts = await fetch('https://damp-anchorage-12027.herokuapp.com/posts');
+  const jsonPosts = await posts.json();
+  return jsonPosts;
+};
+
+// Tipagem para props da home
+export type HomeProps = {
+  posts: PostData[];
+};
+
+// Component Principal
+export default function Home({ posts }: HomeProps) {
   return (
     <div>
-      <h1>Hello World!</h1>
+      {posts.map((post) => (
+        <h2 key={post.slug}>{post.title}</h2>
+      ))}
     </div>
   );
 }
+
+// Implementa o Static Generation (SSG)
+export const getStaticProps: GetStaticProps = async (context) => {
+  const posts = await getPosts();
+  console.log(context);
+
+  return {
+    props: { posts },
+    // revalidate: 60,
+  };
+};

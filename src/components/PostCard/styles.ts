65ea08e8eba13ffe3d3ab3b1e2acb53371a41fd3
TonySilva7 <tony.deve7oper@gmail.com>
2021-08-30 17:59:12 -0300

    feate: create image cards

---
+++ src/components/PostCard/styles.ts
@@ -0,0 +1,26 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  transition: transform 300ms ease;
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+export const PostCardCover = styled.div`
+  margin-bottom: ${({ theme }) => theme.spacings.small};
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
+export const PostCardHeading = styled.h2`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.medium};
+
+    a {
+      color: ${theme.colors.darkGray};
+    }
+  `};
+`;
--- src/containers/HomePage/styles.tsx
+++ src/containers/HomePage/styles.tsx
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const Container = styled.section``;
+export const Container = styled.section`
+  border: solid 1px gray;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  gap: 3rem;
+`;

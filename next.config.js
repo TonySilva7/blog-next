5440054dabc88e2d76ba40e57c69b1b002f55a70
TonySilva7 <tony.deve7oper@gmail.com>
2021-09-07 10:38:40 -0300

    chore: handle next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  target: 'serverless',
   images: {
     domains: ['res.cloudinary.com'],
     deviceSizes: [320, 640, 768, 1024, 1600],
   },
-  // target: 'serverless',
 };
